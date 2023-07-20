d723a8833b098462e49551af97b1a8af6ff1785d
Steven <stevenxchung@gmail.com>
2023-07-19 21:07:34 -0400

    Add styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,28 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in-slow": "fade-in 3s ease-in-out",
+      },
+      backgroundImage: {
+        aman: "url('/background/aman.jpg')",
+        amanGray: "url('/background/aman-grayscale.jpg')",
+      },
+      colors: {
+        "custom-content": "#d99f7e",
+        "custom-div": "#404040",
+      },
+      transitionDuration: {
+        "10s": "10000ms",
+      },
+    },
+    keyframes: {
+      "fade-in": {
+        "0%": { opacity: "0" },
+        "100%": { opacity: "1" },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
