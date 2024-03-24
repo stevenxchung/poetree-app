417cb24aead6b63a6936e4f0dfd9405282a2d137
Steven <stevenxchung@gmail.com>
2024-03-24 09:24:55 -0400

    Update initialize-db.ts

--- initialize-db.ts
+++ initialize-db.ts
@@ -7,17 +7,21 @@ const client = createClient({
   authToken: `${process.env.DATABASE_TOKEN ?? ""}`,
 });
 
-void client.execute(`
-  DROP TABLE PoemOfTheMonth;
-`);
+const initializeDB = async () => {
+  // await client.execute(`
+  //   DROP TABLE PoemOfTheMonth;
+  // `);
 
-void client.execute(`
-  CREATE TABLE PoemOfTheMonth (
-    id TEXT PRIMARY KEY DEFAULT '001',
-    content TEXT,
-    createdAt DATETIME,
-    updatedAt DATETIME
-  );
-`);
+  await client.execute(`
+    CREATE TABLE PoemOfTheMonth (
+      id TEXT PRIMARY KEY DEFAULT '001',
+      content TEXT,
+      createdAt DATETIME,
+      updatedAt DATETIME
+    );
+  `);
+};
+
+void initializeDB();
 
 console.log("Database table(s) created!");
